950b7373b35b4d8dea43e08cb39f6fced31b865f
Ma9lk <63301291+Ma9lk@users.noreply.github.com>
2023-02-10 20:48:32 +0300

    remove unusable imports

--- test_item.py
+++ test_item.py
@@ -1,6 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 def test_product_page_have_add_to_basket_button(browser):
