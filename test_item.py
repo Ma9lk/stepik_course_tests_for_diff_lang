49395ff0b095b31a9e1e4599402363e16479423c
Ma9lk <mayk1909@gmail.com>
2023-02-10 20:31:29 +0300

    add conftest.py and test_item.py

---
+++ test_item.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+def test_product_page_have_add_to_basket_button(browser):
+
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+    browser.get(link)
+
+    add_basket_button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
+
+    assert add_basket_button.is_displayed(), 'Кнопка добавления товара в корзину отсутсвует'
+
+
